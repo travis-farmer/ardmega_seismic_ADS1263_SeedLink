1bd10f09419fb6e894b1cb78c5ebba634873d419
travis-farmer <travis.farmer@outlook.com>
2026-05-15 16:09:52 -0400

    change from 40x4 to I2C 20x4

--- ardmega_seismic_ADS1263_SeedLink.ino
+++ ardmega_seismic_ADS1263_SeedLink.ino
@@ -1,18 +1,15 @@
+#include <avr/wdt.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <ADS126X.h> 
-
-#define LED_DHCP 0
-#define LED_LINK 1
-#define LED_CAL 2
-#define LED_SAMPLING 3
-
-int ledGpins[] = {22, 24, 26, 28}; // Green LEDs for DHCP, Link, Calibration, Sampling
-int ledRpins[] = {23, 25, 27, 29}; // Red LEDs for DHCP, Link, Calibration, Sampling
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 const int W5500_CS = 10;
 const int ADC_CS   = 4;
 const int ADC_DRDY = 9;
+
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xDD, 0xDD };
 EthernetServer server(18000);
@@ -23,67 +20,66 @@ unsigned long nextSampleMicros = 0;
 const unsigned long sampleInterval = 10000; // 10ms in microseconds
 int32_t offsetZ = 0, offsetN = 0, offsetE = 0;
 
+unsigned long lastLcdUpdate = 0;
+const unsigned long lcdInterval = 2000; // Update screen every 2000ms (0.5Hz)
+bool lcdActive = true; // Start with top LCD active
+
 void calibrateOffsets() {
-  //Serial.println("Calibrating True Zero (Keep Station Still)...");
+
   int32_t sumZ = 0, sumN = 0, sumE = 0;
   const int samples = 100;
-  digitalWrite(ledRpins[LED_CAL], HIGH); // Red ON for Calibration
+
   for(int i = 0; i < samples; i++) {
     sumZ += adc.readADC1(ADS126X_AIN0, ADS126X_AIN1);
     sumN += adc.readADC1(ADS126X_AIN2, ADS126X_AIN3);
     sumE += adc.readADC1(ADS126X_AIN4, ADS126X_AIN5);
-    delay(10);
+    delay(5);
   }
   
   offsetZ = sumZ / samples;
   offsetN = sumN / samples;
   offsetE = sumE / samples;
-  digitalWrite(ledRpins[LED_CAL], LOW); // Red OFF after Calibration
-  digitalWrite(ledGpins[LED_CAL], HIGH); // Green ON for Calibration Success
+
+  wdt_reset();
 }
 
 void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(ledGpins[i], OUTPUT);
-    pinMode(ledRpins[i], OUTPUT);
-    digitalWrite(ledGpins[i], LOW);
-    digitalWrite(ledRpins[i], LOW);
-  }
-  // test all LEDs at startup
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(ledGpins[i], HIGH);
-    digitalWrite(ledRpins[i], HIGH);
-    delay(200);
-    digitalWrite(ledGpins[i], LOW);
-    digitalWrite(ledRpins[i], LOW);
-  }
-  //Serial.begin(115200);
-  pinMode(ADC_DRDY, INPUT);
+  wdt_disable();
+  lcd.init();
+  lcd.setCursor(0, 0);
+  lcd.print("                    ");
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+  lcd.setCursor(0, 2);
+  lcd.print("                    ");
+  lcd.setCursor(0, 3);
+  lcd.print("                    ");
+  lcd.setCursor(0, 0);
+  lcd.print("SeisComP SeedLink   ");
 
+  wdt_enable(WDTO_8S);
+
+  pinMode(ADC_DRDY, INPUT);
+  wdt_reset();
   Ethernet.init(W5500_CS); 
   if (Ethernet.begin(mac) == 0) {
-    digitalWrite(ledRpins[LED_DHCP], HIGH); // Red ON for DHCP failure
+    lcd.setCursor(0, 1);
+    lcd.print("DHCP Failed         ");
     if (Ethernet.linkStatus() == LinkOFF) {
-      digitalWrite(ledRpins[LED_LINK], HIGH); // Red ON for Link failure
-    } else {
-      digitalWrite(ledGpins[LED_LINK], HIGH); // Green ON for Link success
+      lcd.setCursor(0, 2);
+      lcd.print("No Link             ");
+      while(1); // Halt if no DHCP and no Link, let user fix the issue
     }
-    // try to configure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip);
   } else {
-    digitalWrite(ledGpins[LED_DHCP], HIGH); // Green ON for DHCP success
-    if (Ethernet.linkStatus() == LinkOFF) {
-      digitalWrite(ledRpins[LED_LINK], HIGH); // Red ON for Link failure
-    } else {
-      digitalWrite(ledGpins[LED_LINK], HIGH); // Green ON for Link success  
-    }
+    lcd.setCursor(0, 1);
+    lcd.print("Good DHCP           ");
   }
   server.begin();
-
+  wdt_reset();
   adc.begin(ADC_CS);
   
   // The library wants (Negative Ref, Positive Ref)
-  adc.setReference(ADS126X_REF_NEG_INT, ADS126X_REF_POS_INT);
+  adc.setReference(ADS126X_REF_NEG_INT, ADS126X_REF_POS_INT); // Using the internal reference for both negative and positive inputs, which is common for single-ended measurements. Adjust if using external reference.
   
   adc.setRate(ADS126X_RATE_1200); 
   adc.setGain(ADS126X_GAIN_32); 
@@ -95,17 +91,14 @@ void setup() {
   // 2. Push the change from the Mega to the ADS1263
   // This writes 1 register starting at address 0x03
   adc.writeRegisters(0x03, 1);
-
+  wdt_reset();
   calibrateOffsets();
 
-  //Serial.println("FARM Station | HHZ 100Hz | External AD580KH Ref Enabled");
   nextSampleMicros = micros();
 }
 
 void loop() {
   EthernetClient client = server.available();
-  digitalWrite(ledGpins[LED_SAMPLING], client.connected() ? HIGH : LOW); // Green ON if client connected
-  digitalWrite(ledRpins[LED_SAMPLING], client.connected() ? LOW : HIGH); // Red ON if no client
   unsigned long currentMicros = micros();
 
   // 100Hz Timer (10,000 microseconds)
@@ -131,4 +124,50 @@ void loop() {
       client.write((uint8_t*)&finalE, 4);
     }
   }
+  wdt_reset();
+
+  if (millis() - lastLcdUpdate >= lcdInterval) {
+    lastLcdUpdate = millis();
+    if (lcdActive) {
+      
+      // We use setCursor and print OVER old data to avoid the blocking lcd.clear()
+      lcd.setCursor(0, 0);
+      lcd.print("SeisComP SeedLink   ");
+      lcd.setCursor(0, 1);
+      lcd.print("IP: ");
+      lcd.print(Ethernet.localIP());
+      
+      lcd.print("      ");   // Clear trailing digits
+      lcd.setCursor(0, 2);
+      if (Ethernet.linkStatus() == LinkOFF) {
+        lcd.print("Link: OFF           ");
+      } else {
+        lcd.print("Link: ON            ");
+      }
+      lcd.setCursor(0, 3);
+      if (client.connected()) {
+        lcd.print("Client              ");
+      } else {
+        lcd.print("No Client           ");
+      }
+      lcdActive = false;
+    } else {
+      lcd.setCursor(0, 0);
+      lcd.print("ADS1263 Cal-Offsets ");
+      lcd.setCursor(0, 1);
+      lcd.print("Z: ");
+      lcd.print(offsetZ);
+      lcd.print("      "); // Clear trailing digits
+      lcd.setCursor(0, 2);
+      lcd.print("N: ");
+      lcd.print(offsetN);
+      lcd.print("      "); // Clear trailing digits
+      lcd.setCursor(0, 3);
+      lcd.print("E: ");
+      lcd.print(offsetE);
+      lcd.print("      "); // Clear trailing digits
+      lcdActive = true;
+    }
+  }
+  wdt_reset();
 }
